2a336108eab4e3804a5dc34bcf2cb0c42744c00b
Juliano Laurentino <66442729+JulianoLaurentino@users.noreply.github.com>
2021-03-19 11:51:08 -0300

    Update hello.dart

--- hello.dart
+++ hello.dart
@@ -14,4 +14,16 @@ main() {
   print(total);
 }
 
+//utilizando o if e else
+main() {
+  var num1 = 3;
+  var num2 = 4;
+  var isTrue = num1 > num2;
+
+  if (isTrue) {
+    print("isso é verdade");
+  } else {
+    print("isso não é verdade");
+  }
+}
 
